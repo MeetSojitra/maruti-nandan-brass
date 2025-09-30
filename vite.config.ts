35b18efededb5dff8b0c39357bad3c792560a595
Meet Sojitra <meetsojitra2783@gmail.com>
2025-09-30 11:51:00 +0530

    c2

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,8 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
+import { viteStaticCopy } from 'vite-plugin-static-copy';
+
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
@@ -13,6 +15,14 @@ export default defineConfig(({ mode }) => ({
     react(),
     mode === 'development' &&
     componentTagger(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'src/assets/*', // copy everything from src/assets
+          dest: 'assets'       // put them in dist/assets
+        }
+      ]
+    })
   ].filter(Boolean),
   resolve: {
     alias: {
