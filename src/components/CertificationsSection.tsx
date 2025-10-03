2c5a0de0d34389594fd22be3990f966d4f7e3aa1
Meet Sojitra <meetsojitra2783@gmail.com>
2025-10-03 16:53:06 +0530

    final 2

--- src/components/CertificationsSection.tsx
+++ src/components/CertificationsSection.tsx
@@ -271,14 +271,14 @@ const CertificationsSection: React.FC = () => {
                 <Download className="w-5 h-5 mr-2" />
                 Download Quality Manual
               </Button>
-              <Button 
+              {/* <Button 
                 size="lg"
                 variant="outline"
                 className="border-primary-foreground text-primary-foreground hover:bg-primary-foreground hover:text-primary"
                 onClick={() => console.log('Request audit report')}
               >
                 Request Audit Report
-              </Button>
+              </Button> */}
             </div>
           </motion.div>
         </motion.div>
